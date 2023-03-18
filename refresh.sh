f02e9cae95afad8d1a2a2a8e1a6683b090f80347
yubanmeiqin9048 <64676973+yubanmeiqin9048@users.noreply.github.com>
2023-03-18 15:50:54 +0800

    Optimize the log and add comments

--- refresh.sh
+++ refresh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 parement=$1
-initpath="${parement/'/Video'/}"
-filename=${initpath##*/}
-alistTarget="/share1${initpath%/*}"
-embyTarget="/mnt/share1${initpath}"
+initpath=${parement/'/Onedrive'/}                       #删除传入路径的多余信息
+filename=${initpath##*/}                                #获取文件名
+alistTarget="/share1${initpath%/*}"                     #拼接为alist对应的网盘目录
+embyTarget="/mnt/share1${initpath/'/Video'/}"           #拼接为emby的媒体库目录
 rcloneTarget="${initpath%/*}"
 log_dir="<your dir>"
-embyUrl="localhost:9187/emby/Library/Media/Updated/"
+embyUrl="localhost:9187/emby/Library/Media/Updated"
 embyToken="<your token>"
 alistUrl="localhost:6355/api/fs/list"
 alistToken="<your token>"
@@ -24,8 +24,7 @@ function rcloneVfsRefresh(){
     while :
     do
         respond=$(rclone rc vfs/refresh dir="${rcloneTarget}")
-        wait
-        isexist=$(echo ${respond} | grep -P -o -w "OK")
+        isexist=$(echo -e ${respond} | grep -P -o -w "OK")
         if [[ -n ${isexist} ]]
         then
             break
@@ -33,7 +32,8 @@ function rcloneVfsRefresh(){
             sleep 7s
         fi
     done
-    echo -e ${respond} | sed 's/\n//g' >> ${log_dir}/rclone.log
+    echo -e "${time}: ${respond}" | tr -d '\t\n' >> ${log_dir}/rclone.log
+    echo "" >> ${log_dir}/rclone.log
     fetchEmbyApi
 }
 
@@ -47,22 +47,21 @@ function fetchAlistPathApi(){
         -H "Authorization:${alistToken}" \
         -H "Content-Type:application/json;charset=utf-8" \
         -d "${body}")
-        wait
         code=$(echo ${respond} | sed 's/,/\n/g' | grep "code" | sed 's/:/\n/g' | sed '1d' | sed 's/}//g')
         message=$(echo ${respond} | sed 's/,/\n/g' | grep "message" | sed 's/:/\n/g' | sed '1d' | sed 's/"//g')
         isexist=$(echo -e ${respond} | grep -P -o -w "${filename}")
-        time=`date +'%Y-%m-%d %H:%M:%S'`
+        echo -e "${time} code: ${code}" >> ${log_dir}/webhook.log
+        echo -e "${time} message: ${message}" >> ${log_dir}/webhook.log
+	    echo -e "${time} isexist: ${isexist}"  >> ${log_dir}/webhook.log
         if [[ "$code" == "200" && -n ${isexist} ]]
         then
-            echo "$respond"
             break
         else
+            echo "${respond}"
             sleep 7s
         fi
     done
     echo -e "time: ${time}" >> ${log_dir}/work.log
-    echo -e "code: ${code}" >> ${log_dir}/work.log
-    echo -e "message: ${message}" >> ${log_dir}/work.log
     echo -e "path: ${initpath}\n\n" >> ${log_dir}/work.log
     sleep 3s
     rcloneVfsRefresh
